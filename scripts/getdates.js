5ffee80e1da4cd901d84ee1cb5a4b71cc9ea86fd
festals <festals90@gmail.com>
2024-06-15 11:29:51 +0200

    create script for date

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+const currentYear = document.querySelector("#currentyear");
+const year = new Date().getFullYear();
+
+
+const lastModfiedDate = document.querySelector("#lastModified");
+currentYear.innerHTML = year;
+
+lastModfiedDate.innerHTML = new Date(document.lastModified);
+
+
+
