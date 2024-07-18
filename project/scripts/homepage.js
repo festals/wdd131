0001960156e3500fa2a3edda7efced2343c4b84e
festals <festals90@gmail.com>
2024-07-18 17:29:37 +0200

    change load

--- project/scripts/homepage.js
+++ project/scripts/homepage.js
@@ -84,7 +84,6 @@ const displayRecipe = (frecipe) => {
     const picture = document.createElement("img");
     picture.setAttribute("src", recipe.imageUrl);
     picture.setAttribute("alt", recipe.name);
-    picture.setAttribute("loading", "lazy");
     slides.setAttribute("class", "mySlides");
 
     section.appendChild(name);
